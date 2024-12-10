756b6391eda3903f9c548f85f0ebd89cea8f7df7
ALIRAZA4278 <af912923@gmail.com>
2024-12-10 15:15:26 +0500

    Update Colours

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="py-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 bg-white text-sm mt-24 ">
+    <div className="py-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 bg-white text-sm mt-24 text-black ">
       <div className="w-full h-px mb-4 bg-gray-200"></div>
       {/* TOP */}
       <div className="flex flex-col md:flex-row justify-between gap-24 items-center">
